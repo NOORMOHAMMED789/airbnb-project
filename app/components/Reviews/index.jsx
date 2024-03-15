a5188b992cd2b4d4e12b4e779cd1571d3294cefb
Noor <noormohammedknr2@gmail.com>
2024-03-15 12:21:06 +0530

    review component in homepage

---
+++ app/components/Reviews/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Reviews({ title, rating, review, tags, place }){
+    return (
+        <div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import Reviews from "./components/Reviews";
 
 export default function Home() {
   return (
     <div>
-      This is the main page
+      <Reviews 
+        title="Bordeaux Gateway"
+        rating={5}
+        review={7}
+        tags="Superhost"
+        place="Bordeaux, France"
+      />
     </div>
   );
 }
