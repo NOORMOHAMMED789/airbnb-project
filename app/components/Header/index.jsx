bb45fa29cfafdb1a8148d5a7ca857de8b04c5c22
Noor <noormohammedknr2@gmail.com>
2024-03-15 11:58:33 +0530

    header is in progress

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Header(){
     return (
-        <div>
-            This is the header
+        <div className='py-[32px] grid grid-cols-3 items-end'>
+            <div>
+                <img src="\Images\mainLogo.png" alt='' width={102} height={32} />  
+            </div>
+            <div>
+                This is the search
+            </div>
+            <div className='absolute right-[160px]'>
+                Become a host
+            </div>
         </div>
     )
 }
