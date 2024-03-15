7aca1198fa0744cd8260fd58f419e2770335bcd3
Noor <noormohammedknr2@gmail.com>
2024-03-15 13:01:04 +0530

    homepage

--- app/components/ImageGallary/index.jsx
+++ app/components/ImageGallary/index.jsx
@@ -1,10 +1,12 @@
+'use client'
+
 import React from 'react'
 
 export default function ImageGallary({}){
     return (
         <div className='flex gap-[8px] w-full'>
-            <div>
-                <img src='\Images\Image.png' width={556} height={400} alt='bedroom-image' className='rounded-l-[24px]'/>
+            <div className=''>
+                <img src='\Images\Image.png' width={566} height={400} alt='bedroom-image' className='rounded-l-[24px]'/>
             </div>
             <div className='flex flex-row gap-[8px]'>
                 <div className='flex gap-[8px] flex-col'>
@@ -12,10 +14,18 @@ export default function ImageGallary({}){
                     <img src='\Images\house2.png' width={274} height={196} alt='house-image' />
                 </div>
                 <div className='flex gap-[8px] flex-col'>
-                    <img src='\Images\house3.png' width={274} height={196} alt='house-image' />
-                    <img src='\Images\house4.png' width={274} height={196} alt='house-image' />
+                    <img src='\Images\house3.png' width={274} height={196} alt='house-image' className='image3' />
+                    <img src='\Images\house4.png' width={274} height={196} alt='house-image' className='image4' />
                 </div>
             </div>
+            <style jsx>{`
+                .image3{
+                    border-top-right-radius: 24px;
+                }
+                .image4{
+                    border-bottom-right-radius: 24px;
+                }
+            `}</style>
         </div>
     )
 }
